fd89d985caaff99f27a3e2c7946d78d91d9c52a5
Mariana Vazquez <mariana.vazquez7033@gmail.com>
2021-12-09 06:42:03 -0500

    react form can save vales onclick submit

--- contactform_container.js
+++ contactform_container.js
@@ -9,24 +9,66 @@ class ContactForm_container extends React.Component {
         }
     }
 
+    updatefName = (event) =>{
+        event.preventDefault();
+        this.setState({
+            fname: event.target.value
+        });
+    }
+    updatelName = (event) =>{
+        event.preventDefault();
+        this.setState({
+            lname: event.target.value
+        });
+    }
+    updateEmail = (event) =>{
+        event.preventDefault();
+        this.setState({
+            email: event.target.value
+        });
+    }
+    updateSubject = (event) =>{
+        event.preventDefault();
+        this.setState({
+            subject: event.target.value
+        });
+    }
+
+    onSubmit = (event) =>{  
+        event.preventDefault();
+        const filledForm = {
+            fname: this.state.fname,
+            lname: this.state.lname,
+            email: this.state.email,
+            subject: this.state.subject
+        }
+        console.log(filledForm);
+        alert("You have made a form and it has been sent to Mariana!")
+
+    }
+
+
     render() {
         return (
         <div>
-            <form netlify name="contactform">
+            <h1>Contact Form React component </h1>
+            <div className="container">
+            <form onSubmit = {this.onSubmit}>
                 <label for="fname">First Name</label>
-                <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+                <input type="text" onChange = {this.updatefName} id="fname" name="firstname" placeholder="Your name..."required/>
 
                 <label for="lname">Last Name</label>
-                <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+                <input type="text" onChange = {this.updatelName} id="lname" name="lastname" placeholder="Your last name..."/>
 
                 <label for="email">Email</label>
-                <input type="text" id="email" name="email" placeholder="Your email.."/>
+                <input type="text" onChange = {this.updateEmail} id="email" name="email" placeholder="Your email..."/>
 
                 <label for="subject">Subject</label>
-                <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+                <textarea id="subject" onChange = {this.updateSubject}name="subject" placeholder="Write something..." ></textarea>
 
-                <input type="submit" value="Submit"/>
+                <input type="submit" value="Submit"required/>
             </form>
+            </div>
          </div>
         )
     }
