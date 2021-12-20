9acdfec05c14affaa9246a310671bbac0e74d8f7
Mariana Vazquez <mariana.vazquez7033@gmail.com>
2021-12-20 03:54:03 -0500

    added weather_app to projects page and will start working on download button on projects page OR animation on home page

--- notif_container.js
+++ notif_container.js
@@ -1,24 +1,24 @@
-class Notif_container extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            fname: '',
-            lanme:'',
-            email: '',
-            subject: ''
-        }
-    }
+// class Notif_container extends React.Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             fname: '',
+//             lanme:'',
+//             email: '',
+//             subject: ''
+//         }
+//     }
 
-    render(){
-        return(
-            <div>
-                <h2>Hello, {this.state.fname  || "Friend"}! 
-                    You have sent a message to me/ my email! I'll get back to you shortly!
-                 </h2>
-                 <p>Thank you for checking out my website!
-                     Yes,the form works :)
-                 </p>
-            </div>
-        )
-    }
-}
+//     render(){
+//         return(
+//             <div>
+//                 <h2>Hello, {this.state.fname  || "Friend"}! 
+//                     You have sent a message to me/ my email! I'll get back to you shortly!
+//                  </h2>
+//                  <p>Thank you for checking out my website!
+//                      Yes,the form works :)
+//                  </p>
+//             </div>
+//         )
+//     }
+// }
